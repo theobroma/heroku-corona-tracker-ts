d0ce49fdec872531c7bcf992b0cd20e48855baac
Alexandr <theobroma222@gmail.com>
2020-09-03 11:49:08 +0300

    upd libs and dotfiles

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,11 +1,23 @@
 // .eslintrc.js
 module.exports = {
   extends: ['airbnb-typescript-prettier', 'plugin:jest/recommended'],
+  plugins: ['react-hooks'],
   rules: {
-    'no-param-reassign': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-unused-vars': 1,
     '@typescript-eslint/no-use-before-define': 0,
+    'import/prefer-default-export': 0,
     'no-console': 0,
+    'no-param-reassign': 0,
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-underscore-dangle': 0,
+    'react/jsx-props-no-spreading': 0,
     'react/state-in-constructor': 0,
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'jsx-a11y/control-has-associated-label': 0,
+    'jsx-a11y/label-has-associated-control': 0,
   },
   env: {
     'jest/globals': true,
